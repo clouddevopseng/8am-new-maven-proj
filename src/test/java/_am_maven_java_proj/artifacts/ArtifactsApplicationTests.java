5d421e0ac4a1a8d2406bff14ad5cdf7ab4b09155
venkat <venkat@tcs.com>
2025-04-07 08:23:23 +0530

    first commit

---
+++ src/test/java/_am_maven_java_proj/artifacts/ArtifactsApplicationTests.java
@@ -0,0 +1,13 @@
+package _am_maven_java_proj.artifacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
